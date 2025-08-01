632b33f8242c4b65802549de08d343b133aeeb1f
newaz sourav <nsourov07@gmail.com>
2025-08-01 23:21:52 +0600

    add start script

--- api/app.js
+++ api/app.js
@@ -15,7 +15,8 @@ const cors = require('cors');
 const app = express();
 app.use(cors({
   origin: 'http://localhost:5173', 
-  credentials: true
+  credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
