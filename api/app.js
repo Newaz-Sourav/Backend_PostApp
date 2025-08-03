800a4741df7aed29bbd6e837a492cc292af28dd3
newaz sourav <nsourov07@gmail.com>
2025-08-03 20:10:48 +0600

    add start script

--- api/app.js
+++ api/app.js
@@ -19,10 +19,7 @@ app.use(cors({
   credentials: true, 
 }));
 
-// app.options('*', cors({
-//   origin: true,
-//   credentials: true,
-// }));
+
 
 
 app.use(express.json());
@@ -89,8 +86,8 @@ app.post('/register', async (req, res) => {
 
                  res.cookie('token', token, {
                  httpOnly: true,
-                  secure: false,      
-                  sameSite: 'Lax',
+                  secure: true,      
+                  sameSite: 'None',
                   maxAge: 7 * 24 * 60 * 60 * 1000      
                   });
 
