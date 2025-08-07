63c7f2a0aee744632a2abf5e7fa6e89b5a326293
newaz sourav <nsourov07@gmail.com>
2025-08-07 19:51:55 +0600

    bug fixed

--- api/app.js
+++ api/app.js
@@ -126,7 +126,8 @@ app.post('/login', async (req, res) => {
       res.cookie('token', token, {
         httpOnly: true,
         sameSite: 'None',
-        secure: true 
+        secure: true,
+        maxAge: 7 * 24 * 60 * 60 * 1000
       });
 
       //res.status(200).redirect('/profile');
